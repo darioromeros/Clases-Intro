8c96ee9be2f9f65481511f6404d6828429a2efa7
Curso Intro <Curso Intro@DESKTOP-5LVH4IU>
2020-09-03 11:24:00 +0200

    test

---
+++ CulpableONo/src/CulpableONo/Principal.java
@@ -0,0 +1,18 @@
+package CulpableONo;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String veredicto = "culpable";
+		
+		if(veredicto == "culpable") {
+			System.out.println("Culpable");
+		} else {
+		System.out.println("Inocente");
+		}
+
+	}
+}

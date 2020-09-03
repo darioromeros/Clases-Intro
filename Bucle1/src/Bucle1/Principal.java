64b3cdcd0947490f10de6410ad5ec4074365f34f
Curso Intro <Curso Intro@DESKTOP-5LVH4IU>
2020-09-03 14:02:24 +0200

    test

---
+++ Bucle1/src/Bucle1/Principal.java
@@ -0,0 +1,14 @@
+package Bucle1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i = 1;i < 11; i++) {
+			System.out.println("3x" + i + "=" + i * 3);
+		}
+		
+	}
+
+}

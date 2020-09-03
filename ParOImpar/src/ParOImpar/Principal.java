d9054b2f1693d0c421edb22f37a1891a41188ca6
Curso Intro <Curso Intro@DESKTOP-5LVH4IU>
2020-09-03 11:28:51 +0200

    test

---
+++ ParOImpar/src/ParOImpar/Principal.java
@@ -0,0 +1,17 @@
+package ParOImpar;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int numero = 12;
+		
+		if(numero%2 == 0) {
+			System.out.println("Es par");
+		} else {
+			System.out.println("Es impar");
+		}
+	}
+
+}

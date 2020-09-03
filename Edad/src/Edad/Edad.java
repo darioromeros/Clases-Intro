0ba1113056741f5e75a646457fc89e41f9a374a6
Curso Intro <Curso Intro@DESKTOP-5LVH4IU>
2020-09-03 10:57:19 +0200

    test

---
+++ Edad/src/Edad/Edad.java
@@ -0,0 +1,17 @@
+package Edad;
+
+public class Edad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int edad = 12;
+		
+		if (edad >= 18) {
+			System.out.println("Es mayor de edad");
+		}	else {
+			System.out.println("Es menor de edad");
+		}
+	}
+
+}

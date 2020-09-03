3e83df81c01bfe697da54995f39edd9950bee83b
Curso Intro <Curso Intro@DESKTOP-5LVH4IU>
2020-09-03 10:52:23 +0200

    test

---
+++ Condicional/src/Condicional/Condicional.java
@@ -0,0 +1,18 @@
+package Condicional;
+
+public class Condicional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean condicion = false;
+		
+		if (condicion) {
+			System.out.println();
+		}	else {
+			
+			System.out.println("Esto NO se ejecuta");
+		}
+	}
+
+}

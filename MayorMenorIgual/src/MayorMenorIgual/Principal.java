e5c85aee4f4e550b5dc6b851dd9b975d741de34c
Curso Intro <Curso Intro@DESKTOP-5LVH4IU>
2020-09-03 12:06:44 +0200

    test

---
+++ MayorMenorIgual/src/MayorMenorIgual/Principal.java
@@ -0,0 +1,20 @@
+package MayorMenorIgual;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int primero = 10;
+		int segundo = 23;
+		
+		if(primero == segundo) {
+			System.out.println("Son iguales");
+		} else if(primero > segundo) {
+			System.out.println("El primero es mayor");
+		} else {
+			System.out.println("El primero es menor");
+		}
+	}
+
+}

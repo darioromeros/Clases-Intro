17028dc26e737aef118e165c79612027a85b80a3
Curso Intro <Curso Intro@DESKTOP-5LVH4IU>
2020-09-03 11:10:22 +0200

    test

--- Edad/src/Edad/Edad.java
+++ Edad/src/Edad/Edad.java
@@ -7,7 +7,7 @@ public class Edad {
 
 		int edad = 12;
 		
-		if (edad >= 18) {
+		if (edad > 18 && edad == 18) {
 			System.out.println("Es mayor de edad");
 		}	else {
 			System.out.println("Es menor de edad");

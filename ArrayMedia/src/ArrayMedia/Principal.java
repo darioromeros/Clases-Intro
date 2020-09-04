925b9b308f784d250db70b2905ab6b865e8e734e
Curso Intro <Curso Intro@DESKTOP-5LVH4IU>
2020-09-04 11:39:19 +0200

    test

---
+++ ArrayMedia/src/ArrayMedia/Principal.java
@@ -0,0 +1,17 @@
+package ArrayMedia;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int miArray[] = {2,3,4,5,6,7};
+		
+		for (int posicion = 0; posicion < miArray.length; posicion++) {
+			
+		}
+		
+		System.out.println("La media es ");
+	}
+
+}

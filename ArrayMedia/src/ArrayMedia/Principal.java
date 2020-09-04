c50f1c8a1754d147afee6040a03b1518f9950a7c
Curso Intro <Curso Intro@DESKTOP-5LVH4IU>
2020-09-04 11:47:29 +0200

    test

--- ArrayMedia/src/ArrayMedia/Principal.java
+++ ArrayMedia/src/ArrayMedia/Principal.java
@@ -6,12 +6,16 @@ public class Principal {
 		// TODO Auto-generated method stub
 
 		int miArray[] = {2,3,4,5,6,7};
+		int sumatorio = 0;
+		
 		
 		for (int posicion = 0; posicion < miArray.length; posicion++) {
-			
+			sumatorio = sumatorio + miArray[posicion];
 		}
 		
-		System.out.println("La media es ");
+		double media = sumatorio / miArray.lenght;
+
+		System.out.println("La media es: " + media);
 	}
 
 }

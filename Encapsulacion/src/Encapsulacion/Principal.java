aeb39e670ba3dc2626792b2ef37dfa7c266ddca4
Curso Intro <Curso Intro@DESKTOP-5LVH4IU>
2020-09-09 11:21:45 +0200

    test

--- Encapsulacion/src/Encapsulacion/Principal.java
+++ Encapsulacion/src/Encapsulacion/Principal.java
@@ -5,6 +5,21 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//Crear objeto usuario
+		
+		Usuario miUsuario = new Usuario("Daniel");
+		
+		
+		//Imprimir estado del objeto
+		
+		System.out.println(miUsuario);
+		
+		System.out.println(miUsuario.activado);
+		
+		//Activacion de usuario
+		
+		miUsuario.activarUsuario();
+		
 		
 	}
 
--- Encapsulacion/src/Encapsulacion/Usuario.java
+++ Encapsulacion/src/Encapsulacion/Usuario.java
@@ -5,6 +5,32 @@ import java.util.Date;
 public class Usuario {
 
 	public String nombre;
-	public boolean activado;
+	private boolean activado;
 	public Date fechaActivacion;
+	
+	public Usuario(String nombre) {
+		
+		this.nombre = nombre;
+		this.activado = false;
+	}
+	
+	String mensaje = "Usuario: " + this.nombre + ""{
+		
+		if(this.activado) {
+			return mensaje + "fecha de activacion: " + this.fechaActivacion;
+		}
+		
+		return mensaje;
+	}
+	
+	//metodo getter
+	public boolean getActivado() {
+		return this.activado;
+	}
+	
+	//metodo setter
+	public void activarUsuario() {
+		this.activado = true;
+		this.fechaActivacion = new date();
+	}
 }
